00ef1ff4c10417465058d8cf238ec381709c4517
Elie Ruvinga <ruvingaelie@gmail.com>
2023-01-11 04:59:01 +0100

    add the leading page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; 
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
---
+++ styles/colors/index.js
@@ -0,0 +1,10 @@
+
+const Color = {
+    primary: '#5843E4',
+    backgroundLight: '#F9F9FC',
+    Headtext: '#2F2E41',
+    Navtext: '#8186A0',
+    Shadow: "#E2E3E9"
+} 
+
+export default Color;
