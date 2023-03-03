0e69a33019214eefea4a09166ff44c9604e8367f
ERuvinga <ruvingaelie@gmail.com>
2023-03-03 23:49:52 +0200

    implemente a function that control if user is Authentification for access to Chat page

---
+++ components/WithAuth/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { NextPage } from "next";
+
+//this component check a if user as login
+
+const WithAuth = (Page: NextPage) => {
+
+};
+
+export default WithAuth;
