d838039c5f524088feb5c70d5f24bd968d4d8eb9
ERuvinga <ruvingaelie@gmail.com>
2023-03-23 16:39:18 +0200

    adding style and change api_link to remote_api_Link

--- components/CommonComponents/Chat/DescriptionUser/index.tsx
+++ components/CommonComponents/Chat/DescriptionUser/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMessage, faUserFriends, faHeartCirclePlus, faPhone, faCircle, faUserEdit, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { faMessage, faUserFriends, faHeartCirclePlus, faPhone, faCircle, faUserEdit } from '@fortawesome/free-solid-svg-icons';
 
 
 interface friendDesciption {
