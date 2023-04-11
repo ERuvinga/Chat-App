fe93c986390b09e35f4c1620a14d10567ff01a9b
ERuvinga <ruvingaelie@gmail.com>
2023-04-11 22:35:40 +0200

    updated socket.io

--- components/Context/socket.js
+++ components/Context/socket.js
@@ -2,8 +2,8 @@ import { createContext, useState } from 'react';
 export const socketIoContext = createContext(null);
 
 function SocketContext({ children }) {
-    const [io, setIo] = useState(null);
-    return <socketIoContext.Provider value={{ io, setIo }}>{children}</socketIoContext.Provider>
+    const [socketIo, setIo] = useState(null);
+    return <socketIoContext.Provider value={{ socketIo, setIo }}>{children}</socketIoContext.Provider>
 }
 
 export default SocketContext;
