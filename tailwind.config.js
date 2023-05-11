ed22289089404897ed32e384b2b26fc12b6c1d46
ERuvinga <ruvingaelie@gmail.com>
2023-05-11 14:16:29 +0200

    adding other endpoints and add style to responsivess

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'md_lg': '1320px',
+        // => @media (min-width: 1050px) { ... }
+        'mobile': '370px',
+        // => @media (min-width: 320px) { ... }
+
+        'TabletPoint': '920px',
+        // => @media (min-width: 320px) { ... }
+        'sm': '530px'
+      },
+
+    },
   },
   plugins: [],
 }
