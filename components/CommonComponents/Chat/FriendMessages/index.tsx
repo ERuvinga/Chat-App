133c86a5005828c4ca3013f698b2214ea558c5cf
ERuvinga <ruvingaelie@gmail.com>
2023-05-15 20:30:18 +0200

    fixed position elements

--- components/CommonComponents/Chat/FriendMessages/index.tsx
+++ components/CommonComponents/Chat/FriendMessages/index.tsx
@@ -26,7 +26,7 @@ const Index = (datas: NewMessages) => {
     ioContext = useContext(socketIoContext);
 
     return (
-        <div className=' flex flex-row w-[80px] h-[80px] TabletPoint:h-[100%] TabletPoint:w-[95%] TabletPoint:justify-center Container-user  TabletPoint:items-center TabletPoint:my-3'
+        <div className=' flex flex-row w-[64px] h-[64px] TabletPoint:h-[100%] TabletPoint:w-[95%] TabletPoint:justify-center TabletPoint:items-center TabletPoint:my-3'
             onClick={() => {
                 ChatContxt.setLoadingMessage(true);
                 ChatContxt.setSelectedUser(datas.indexUser);
@@ -61,7 +61,7 @@ const Index = (datas: NewMessages) => {
             }
             } >
             <div className={(ChatContxt.selectedUser == datas.indexUser) ? 'ContainerUserSelected flex flex-col justify-center items-center space-y-[2px] TabletPoint:space-y-0 TabletPoint:flex-row TabletPoint:justify-center' : 'ContainerMessage flex flex-col justify-center items-center space-y-[2px] TabletPoint:space-y-0 TabletPoint:flex-row TabletPoint:justify-between'}>
-                <img src={datas.picture ? datas.picture : 'profile.png'} className=' w-[35px] h-[35px] TabletPoint:w-[50px] TabletPoint:h-[50px] imgUserMessage ' alt='user' />
+                <img src={datas.picture ? datas.picture : 'profile.png'} className=' w-[28px] h-[28px] TabletPoint:w-[50px] TabletPoint:h-[50px] imgUserMessage ' alt='user' />
                 <p className='w-[100%] flex flex-col items-center justify-center TabletPoint:items-start TabletPoint:w-[60%] TabletPoint:ml-[8px] '>
                     <span className=' text-[.6em] w-[80%] truncate TabletPoint:text-[.8em] UsersendMessage'>{datas.name}</span>
                     <span className='hidden TabletPoint:flex messages'>{datas.contentMessage}</span>
