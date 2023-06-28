06500a79ee6a4bca650f047da207e1c90a76fced
ERuvinga <ruvingaelie@gmail.com>
2023-06-28 21:33:41 +0200

    add style to error log notification on login and register pages

--- components/CommonComponents/ErrorLog/index.tsx
+++ components/CommonComponents/ErrorLog/index.tsx
@@ -8,7 +8,7 @@ const ErrorLog = () => {
     errorDatas = useContext(ContextUser);
 
     return( 
-        <div className={(errorDatas.data.stateError) ? "ErrorLog flex space-x-3": " hidden"}>
+        <div className={(errorDatas.data.stateError) ? "ErrorLog flex space-x-1": " hidden"}>
             <FontAwesomeIcon icon={faCircleInfo} className="text-[#8186A0]"/>
             <span className= "ErrorMsg text-[#E2E3E9] " > 
                 # {errorDatas.data.MessageError}
